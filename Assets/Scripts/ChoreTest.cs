4346fbbf4c76bb2230af3c9740f37a8389849179
hyun2ee <62418673+hyun2ee@users.noreply.github.com>
2021-03-22 17:12:39 +0900

    20.01.05 iOS 전환용

---
+++ Assets/Scripts/ChoreTest.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChoreTest : MonoBehaviour
+{
+    private static ChoreTest _instance;
+    public static ChoreTest instance {
+        get {
+            if (_instance != null) return _instance;
+
+            _instance = FindObjectOfType<ChoreTest>();
+
+            if (_instance == null)
+                _instance = new GameObject("Testobj").AddComponent<ChoreTest>();
+
+            return _instance;
+        }
+    }
+
+    //string testst = "LIKE_Bose<2";
+
+    private IEnumerator Start() {
+        yield return new WaitForSeconds(2);
+        test();
+    }
+
+    void test() {
+        int[] intarr = new int[10];
+        int[] intarr2 = new int[] { 0, 1, 2, 3, 4, 5 };
+        intarr = intarr2;
+        Debug.Log("test - length of intarr: " + intarr.Length);
+    }
+}
